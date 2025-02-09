a5a725273c295a6ff00e9e269a543b4847a76fe9
Rodrigo Freitas <rsfreitas@somatech.app>
2025-02-09 18:31:57 -0300

    fix: adjust HTTP service defs loading

--- internal/services/http/definition.go
+++ internal/services/http/definition.go
@@ -9,16 +9,15 @@ import (
 )
 
 type Definitions struct {
-	DisableAuth          bool `toml:"disable_auth,omitempty" default:"false"`
-	DisablePanicRecovery bool `toml:"disable_panic_recovery,omitempty" default:"false"`
-	HideErrorDetails     bool `toml:"hide_error_details,omitempty"`
+	DisableAuth          bool `toml:"disable_auth,omitempty" default:"false" json:"disable_auth"`
+	DisablePanicRecovery bool `toml:"disable_panic_recovery,omitempty" default:"false" json:"disable_panic_recovery"`
 }
 
 func newDefinitions(definitions *definition.Definitions) *Definitions {
 	if currentDefs, ok := definitions.LoadService(definition.ServiceType_HTTP); ok {
 		if b, err := json.Marshal(currentDefs); err == nil {
 			var serviceDefs Definitions
-			if err := json.Unmarshal(b, &serviceDefs); err != nil {
+			if err := json.Unmarshal(b, &serviceDefs); err == nil {
 				return &serviceDefs
 			}
 		}
